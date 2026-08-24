f074f6d91cd06d4e65be532648ccce7e2c035fa2
Andre Ugolini <andreugolini@unochapeco.edu.br>
2026-08-24 13:59:44 -0300

    chore: add comments

--- internal/git/git.go
+++ internal/git/git.go
@@ -72,6 +72,7 @@ func parseStatus(out string) ([]FileStatus, error) {
 	return files, nil
 }
 
+// Add stages the given path
 func Add(dir, path string) error {
 	cmd := exec.Command("git", "add", "--", path)
 	cmd.Dir = dir
@@ -82,6 +83,7 @@ func Add(dir, path string) error {
 	return nil
 }
 
+// AddAll stages all path
 func AddAll(dir string) error {
 	cmd := exec.Command("git", "add", "-A")
 	cmd.Dir = dir
@@ -92,6 +94,7 @@ func AddAll(dir string) error {
 	return nil
 }
 
+// Reset unstages the given path, leaving working tree changes intact.
 func Reset(dir, path string) error {
 	cmd := exec.Command("git", "reset", "--", path)
 	cmd.Dir = dir
@@ -102,6 +105,7 @@ func Reset(dir, path string) error {
 	return nil
 }
 
+// Untracked reports whether this file is untracked by git.
 func (f FileStatus) Untracked() bool {
 	return f.Index == '?' && f.Worktree == '?'
 }
@@ -111,6 +115,8 @@ func (f FileStatus) Staged() bool {
 	return f.Index != ' ' && f.Index != '?'
 }
 
+// Unstaged reports whether this entry has changes not yet staged
+// (including being an untracked file).
 func (f FileStatus) Unstaged() bool {
 	return f.Worktree != ' ' || (f.Index == '?' && f.Worktree == '?')
 }
